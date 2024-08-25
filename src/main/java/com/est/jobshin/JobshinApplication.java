be81e01c3238b190b6f69daf5d82bc965b158989
JeongSeok <ljs0429777@gmail.com>
2024-08-25 14:40:59 +0900

    Feat: 프로젝트 생성

---
+++ src/main/java/com/est/jobshin/JobshinApplication.java
@@ -0,0 +1,13 @@
+package com.est.jobshin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobshinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobshinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/est/jobshin/JobshinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.est.jobshin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobshinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
