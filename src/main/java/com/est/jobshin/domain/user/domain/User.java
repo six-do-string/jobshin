0786d33337920f9df72c0b987ddc462ca40e9bba
jojunho <junhosvain@naver.com>
2024-08-26 17:46:59 +0900

    feat: User domain 및 관련(Enum, BaseEntity) 클래스 작성

--- src/main/java/com/est/jobshin/domain/user/domain/User.java
+++ src/main/java/com/est/jobshin/domain/user/domain/User.java
@@ -1,15 +1,51 @@
 package com.est.jobshin.domain.user.domain;
 
+import com.est.jobshin.domain.user.util.Language;
+import com.est.jobshin.domain.user.util.Level;
+import com.est.jobshin.domain.user.util.Position;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "username")
+	private String username;
 
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "nickname")
+	private String nickname;
+
+	@Column(name = "language")
+	@Enumerated(EnumType.STRING)
+	private Language language;
+
+	@Column(name = "level")
+	@Enumerated(EnumType.STRING)
+	private Level level;
+
+	@Column(name = "position")
+	@Enumerated(EnumType.STRING)
+	private Position position;
 }
---
+++ src/main/java/com/est/jobshin/domain/user/util/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.est.jobshin.domain.user.util;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
