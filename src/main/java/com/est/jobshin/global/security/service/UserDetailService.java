97c7d2d405121c7c5fdfbee3177f3e9384027e86
jojunho <junhosvain@naver.com>
2024-08-27 01:23:55 +0900

    feat: Security Details, DetailService 파일 작성

---
+++ src/main/java/com/est/jobshin/global/security/service/UserDetailService.java
@@ -0,0 +1,27 @@
+package com.est.jobshin.global.security.service;
+
+import com.est.jobshin.domain.user.domain.User;
+import com.est.jobshin.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByemail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
