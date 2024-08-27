682d855c3a97bd5caee58b8e8c353ab3b80f83c5
jojunho <junhosvain@naver.com>
2024-08-27 11:09:55 +0900

    feat: 409 에러(서버의 현재 상태와 요청이 충돌) 추가 - 이메일 중복 검사 에러

--- src/main/java/com/est/jobshin/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/est/jobshin/global/error/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.est.jobshin.global.error;
 
 import com.est.jobshin.global.common.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +14,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.NOT_VALID_ERROR, e.getBindingResult());
         return ResponseEntity.badRequest().body(errorResponse);
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.EMAIL_ALREADY_EXISTS, e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 
 }
