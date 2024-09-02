be10a81fbac9cb69f8e76827f4e1128a357b33f7
JeongSeok <ljs0429777@gmail.com>
2024-09-03 01:29:51 +0900

    Feat: 실전 모드 상세보기 DTO 개발

---
+++ src/main/java/com/est/jobshin/domain/user/dto/MyPageInterviewDetailDto.java
@@ -0,0 +1,41 @@
+package com.est.jobshin.domain.user.dto;
+
+import com.est.jobshin.domain.interviewDetail.domain.InterviewDetail;
+import com.est.jobshin.domain.interviewDetail.util.Category;
+import com.est.jobshin.domain.interviewDetail.util.Mode;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyPageInterviewDetailDto {
+
+	private Long id;
+	private String question;
+	private String answer;
+	private Category category;
+	private Mode mode;
+	private double score;
+	private String exampleAnswer;
+	private LocalDateTime createdAt;
+
+	public static MyPageInterviewDetailDto fromInterviewDetail(InterviewDetail interviewDetail) {
+		return MyPageInterviewDetailDto.builder()
+			.id(interviewDetail.getId())
+			.question(interviewDetail.getQuestion())
+			.answer(interviewDetail.getAnswer())
+			.category(interviewDetail.getCategory())
+			.mode(interviewDetail.getMode())
+			.score(interviewDetail.getScore())
+			.exampleAnswer(interviewDetail.getExampleAnswer())
+			.createdAt(interviewDetail.getCreatedAt())
+			.build();
+	}
+}
