a91d09be7d5946024045ae2fd9c4f12e08bc60e6
jojunho <junhosvain@naver.com>
2024-09-05 22:12:07 +0900

    Remove: Post 폴더 및 하위 파일 삭제

--- src/main/java/com/est/jobshin/domain/posts/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.est.jobshin.domain.posts.repository;
-
-import com.est.jobshin.domain.posts.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
